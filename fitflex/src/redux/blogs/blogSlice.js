5ae69b2fda14f26a85392871d4b86f3e9b79b998
sd <dasarwarshubham1@gmail.com>
2023-08-11 14:29:20 +0530

    redux blogSlice code refactor

--- fitflex/src/redux/blogs/blogSlice.js
+++ fitflex/src/redux/blogs/blogSlice.js
@@ -1,6 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { retrieveBlogs, getBlogById } from "./blogActions";
 
+const isPendingAction = (action) => {
+  return action.type.startsWith(`blogs/`) && action.type.endsWith("/pending");
+};
+const isRejectedAction = (action) => {
+  return action.type.startsWith(`blogs/`) && action.type.endsWith("/rejected");
+};
+
 const blogListSlice = createSlice({
   name: "blogs",
   initialState: {
@@ -11,29 +18,23 @@ const blogListSlice = createSlice({
   },
   extraReducers: (builder) => {
     builder
-      .addCase(retrieveBlogs.pending, (state) => {
-        state.loading = true;
-      })
       .addCase(retrieveBlogs.fulfilled, (state, action) => {
         state.loading = false;
         state.blogList = action.payload;
-        state.error = null;
-      })
-      .addCase(retrieveBlogs.rejected, (state, action) => {
-        state.loading = false;
-        state.error = action.error.message;
-      })
-      .addCase(getBlogById.pending, (state) => {
-        state.loading = true;
       })
       .addCase(getBlogById.fulfilled, (state, action) => {
         state.loading = false;
         state.blogDetails = action.payload;
-        state.error = null;
       })
-      .addCase(getBlogById.rejected, (state, action) => {
+      .addMatcher(isPendingAction, (state) => {
+        state.loading = true;
+      })
+      .addMatcher(isRejectedAction, (state, action) => {
         state.loading = false;
         state.error = action.error.message;
+      })
+      .addDefaultCase((state) => {
+        return state;
       });
   },
 });
