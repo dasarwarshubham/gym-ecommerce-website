9623ebc68daaca2abd578e55fe49d377bdfa429d
sd <dasarwarshubham1@gmail.com>
2023-08-15 23:31:15 +0530

    Shipping Page: Card check icon added

--- fitflex/src/components/cards/checkout/AddressFormRadio.js
+++ fitflex/src/components/cards/checkout/AddressFormRadio.js
@@ -1,9 +1,26 @@
 import React from "react";
 import FormCheck from "react-bootstrap/FormCheck";
 import Feedback from "react-bootstrap/Feedback";
+import { MdCheckCircle } from "react-icons/md";
 
 import { useFormikContext } from "formik";
-import { Card } from "react-bootstrap";
+import { Card as BsCard } from "react-bootstrap";
+
+import styled from "styled-components/macro";
+
+const Card = styled(BsCard)`
+  width: 100%;
+  /* background-color: ${(props) =>
+    props.checked ? "#f8f9fa" : "transparent"}; */
+  border: 3px solid
+    ${(props) => (props.checked ? "#111111" : "rgba(0, 0, 0, 0.175)")};
+  position: relative;
+  .mdCheck {
+    position: absolute;
+    right: 0.5rem;
+    top: 0.5rem;
+  }
+`;
 
 const AddressFormRadio = ({
   label,
@@ -19,6 +36,7 @@ const AddressFormRadio = ({
   ...otherProps
 }) => {
   const { values, errors, touched, setFieldValue } = useFormikContext();
+  const isChecked = value.id === values[name].id;
   return (
     <FormCheck type={type} id={`check-api-${type}`} className="p-0">
       <FormCheck.Input
@@ -27,7 +45,7 @@ const AddressFormRadio = ({
         name={name}
         onChange={() => setFieldValue(name, value)}
         isInvalid={touched[name] && !!errors[name]}
-        checked={value.id === values[name].id}
+        checked={isChecked}
         id={`shipping-address-${value.id}`}
         key={`shipping-address-${value.id}`}
         aria-label={`address-${value.id}`}
@@ -37,11 +55,7 @@ const AddressFormRadio = ({
         htmlFor={`shipping-address-${value.id}`}
         className="w-100"
       >
-        <Card
-          className={`w-100 border-3 ${
-            value.id === values[name].id && "border border-primary bg-light"
-          }`}
-        >
+        <Card checked={isChecked}>
           <Card.Body>
             <Card.Title>{value.fullName}</Card.Title>
             <Card.Text>{value.addressLine1}</Card.Text>
@@ -51,6 +65,7 @@ const AddressFormRadio = ({
             </Card.Text>
             <Card.Text>Phone: {value.phone}</Card.Text>
           </Card.Body>
+          {isChecked && <MdCheckCircle className="mdCheck" size={20} />}
         </Card>
       </FormCheck.Label>
       <Feedback type="invalid">{errors[name]}</Feedback>
