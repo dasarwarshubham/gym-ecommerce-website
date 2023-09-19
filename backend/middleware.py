4da44169a5f1394d2034b0dfc600188904784e24
sd <dasarwarshubham1@gmail.com>
2023-09-19 08:38:07 +0530

    csrftoken for admin routes only

---
+++ backend/middleware.py
@@ -0,0 +1,17 @@
+from django.middleware.csrf import get_token
+
+class ConditionalCsrfMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if request.path.startswith('/admin/'):
+            # Enable CSRF protection for admin routes
+            get_token(request)
+        else:
+            # Disable CSRF for all other routes
+            setattr(request, '_dont_enforce_csrf_checks', True)
+
+        response = self.get_response(request)
+
+        return response
--- backend/settings/common.py
+++ backend/settings/common.py
@@ -91,6 +91,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'backend.middleware.ConditionalCsrfMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
